47a5cc499f90493fd54ca4632a8f73d3b008ab91
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-29 04:40:19 +0300

    u

---
+++ js/swiper.js
@@ -0,0 +1,54 @@
+
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+const slider = document.querySelector(".swiper");
+
+const swiper = new Swiper(slider, {
+    
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "1",
+    autoplay: true,
+   
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+        
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 300,
+        speed: 2000,
+        grabCursor: true,
+        loop: true,
+        slidesPerView: 1,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+        },
+      },
+  
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        slideToClickedSlide: true,
+        centeredSlides: true,
+        grabCursor: true,
+      },
+  
+      1200: {
+        delay:3,
+        slidesPerView: 1,
+        spaceBetween: 100,
+        speed: 1500,
+        slideToClickedSlide: true,
+        // grabCursor: true,
+        loop: true,
+      },
+    },
+  });
