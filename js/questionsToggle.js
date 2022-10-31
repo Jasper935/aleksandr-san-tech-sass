52a6a3767f2a8f47f3fc690aa03a725b13e463ca
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-31 22:46:03 +0200

    u

--- js/questionsToggle.js
+++ js/questionsToggle.js
@@ -1,12 +1,22 @@
 const btn = document.querySelector('.questions-btn')
 const text = document.querySelector('.questions-text')
-btn.addEventListener('click', () => {
-    btn.classList.contains('questions-btn--open') ?
-        btn.classList.remove('questions-btn--open') :
-        btn.classList.add('questions-btn--open')
+
+const textIdStr ='text'
+
+const btns=[...document.querySelectorAll('.questions-btn')]
+btns.forEach((item, index) => {
+    item.addEventListener('click', e => {
+        id=e.target.id
         
-        text.classList.contains('questions-text--hidden') ?
-        text.classList.remove('questions-text--hidden') :
-        text.classList.add('questions-text--hidden')
-    
-})
+       const currentBtn= document.querySelector(`#${id}`)
+       
+       const currentText=  document.querySelector(`#${textIdStr+index}`)
+        if (currentBtn.classList.contains("questions-btn--open")) {
+            currentBtn.classList.remove("questions-btn--open")
+            currentText.classList.add('questions-text--hidden')
+            return
+        }
+        currentBtn.classList.add("questions-btn--open")
+        currentText.classList.remove('questions-text--hidden')
+  });
+});
