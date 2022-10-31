8f86169f6f3c17f61ffbc12656ee5cd4bffeacce
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-31 20:21:46 +0200

    u

--- js/questionsToggle.js
+++ js/questionsToggle.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('.questions-btn')
+const text = document.querySelector('.questions-text')
 btn.addEventListener('click', () => {
-    if (btn.classList.contains('questions-btn--open')) {
-        btn.classList.remove('questions-btn--open')
-        return
-    }
-    btn.classList.add('questions-btn--open')
-    // return
+    btn.classList.contains('questions-btn--open') ?
+        btn.classList.remove('questions-btn--open') :
+        btn.classList.add('questions-btn--open')
+        
+        text.classList.contains('questions-text--hidden') ?
+        text.classList.remove('questions-text--hidden') :
+        text.classList.add('questions-text--hidden')
+    
 })
