41e541f8262745b11f60117997d31009daeff8ca
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-03 04:07:48 +0200

    u

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const list = document.querySelector('.reviews-list')
 const form = document.querySelector("#reviews-form")
 form.addEventListener('submit', onSubmit)
 const btn = document.querySelector('.reviews-btn')
-
+const date =new Date()
+const currentDate = `${date.getDate()}.${date.getMonth()}
+.${date.getFullYear() } ${date.getHours()<10?'0'+date.getHours():date.getHours()}:${date.getMinutes()}`
 fetchReviews().then((res) => { createMarkUp(res) })
 
-
+console.log(date.getDate());
 async function fetchReviews() {
   return await fetch('https://6348b82ea59874146b0f8c0a.mockapi.io/reviews').then((res) => res.json())
 }
 
 
 async function setReview(obj = {}) {
- 
+
   return fetch('https://6348b82ea59874146b0f8c0a.mockapi.io/reviews', {
     method: "post", headers: {
       'Content-Type': 'application/json'
@@ -25,9 +27,10 @@ async function setReview(obj = {}) {
 
 async function onSubmit(e) {
   e.preventDefault()
-  const obj = { name: e.target.name.value, email: e.target.email.value, message: e.target.message.value }
+  const obj = { name: e.target.name.value, email: e.target.email.value,
+     message: e.target.message.value, date: currentDate }
   await setReview(obj)
-  await fetchReviews().then(res=>createMarkUp(res))
+  await fetchReviews().then(res => createMarkUp(res))
   form.reset()
 }
 
@@ -51,7 +54,7 @@ function createMarkUp(res) {
     
 </li>`).reverse().join('')
 
-  list.innerHTML=markUp
+  list.innerHTML = markUp
   // const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
 }
