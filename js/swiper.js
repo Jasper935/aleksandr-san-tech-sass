5c640776ddea279508d4a68e0ec843607efbaa41
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-04 17:58:12 +0200

    u

--- js/swiper.js
+++ js/swiper.js
@@ -52,3 +52,57 @@ const swiper = new Swiper(slider, {
       },
     },
   });
+
+
+  const sliderBot = document.querySelector(".swiper");
+
+const swiperBot = new Swiper(sliderBot, {
+    
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "1",
+    autoplay: true,
+   
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+        
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 300,
+        speed: 2000,
+        grabCursor: true,
+        loop: true,
+        slidesPerView: 1,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+        },
+      },
+  
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        slideToClickedSlide: true,
+        centeredSlides: true,
+        grabCursor: true,
+      },
+  
+      1200: {
+        delay:3,
+        slidesPerView: 1,
+        spaceBetween: 50,
+        speed: 1500,
+        slideToClickedSlide: true,
+        // grabCursor: true,
+        loop: true,
+      },
+    },
+  });
