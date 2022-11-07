2bf31c6b32433bd59bdc58cebc67ea46f04fb897
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-07 03:58:37 +0200

    u

---
+++ js/footerModal.js
@@ -0,0 +1,27 @@
+const toggleButton = document.querySelector('.footer-span')
+const toggleContent = document.querySelector('.made-by')
+const closeBtn = document.querySelector('.made-by_close_btn')
+
+
+
+toggleButton.addEventListener('click', () => {
+    if (toggleContent.classList.contains('made-by_hidden')) {
+        toggleContent.classList.remove('made-by_hidden')
+        toggleButton.classList.add("bottom-side_button--open")
+
+    } else {
+
+        toggleContent.classList.add('made-by_hidden')
+        toggleButton.classList.remove("bottom-side_button--open")
+
+    }
+})
+closeBtn.addEventListener('click', () => {
+    toggleContent.classList.add('made-by_hidden')
+    toggleButton.classList.remove("bottom-side_button--open")
+})
+
+// window.addEventListener("click", () => {
+//     toggleContent.classList.add('made-by_hidden')
+
+// })
